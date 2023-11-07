f2d387783449561d29a9847625b1929bed9b9d1e
ratanpandey1994 <42265582+ratanpandey1994@users.noreply.github.com>
2023-11-07 09:46:53 +0530

    7 Nov

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head>
+  <meta charset="<?php bloginfo('charset') ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
-<body>
+<body class="<?php body_class(); ?>">
 
 <header class="site-header">
       <div class="container">

878bec82a65373b19a27adeb0164044f8ccefedc
ratanpandey1994 <42265582+ratanpandey1994@users.noreply.github.com>
2023-11-14 21:58:54 +0530

    14 Nov Latest

--- mu-plugins/custom-post-types.php
+++ mu-plugins/custom-post-types.php
@@ -2,6 +2,10 @@
 // wp-content => mu-plugins
 function event_post_types() {
     register_post_type('event', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
         'public' => true,
         'labels' => array(
             'name' => 'Events',
